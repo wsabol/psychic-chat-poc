ac615c77ba6836eef122ec1ee2a0ddfb406f9043
Will Sabol <wsabol39@gmail.com>
2025-09-30 05:49:52 -0500

    railway config

--- worker/shared/queue.ts
+++ worker/shared/queue.ts
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-const client = createClient({ url: process.env.REDIS_URL || '' });
+const client = createClient({ url: process.env.REDIS_URL || 'redis://redis:6379' });
 client.connect();
 
 export async function getMessageFromQueue() {
