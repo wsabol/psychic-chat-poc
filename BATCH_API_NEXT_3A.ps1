4f6334a92f2b18cf33fabdbc3a620488576d150f
Stuart <stuathome87@gmail.com>
2026-01-15 22:41:13 -0600

    Revert "Fix console.error/warn: api/shared/sessionManager/services/fraudDetectionService.js"
    This reverts commit fe2afd2dfe9e45c1da6ccce6f5e053adb751d877.

--- BATCH_API_NEXT_3A.ps1
+++
@@ -1,8 +0,0 @@
-# Next 3 API files
-$Files = @(
-    "api/shared/auditLog.js",
-    "api/shared/queue.js",
-    "api/shared/redis.js"
-)
-
-& .\BULK_CONSOLE_FIX_V4.ps1 -Files $Files -BatchName "API_REMAINING_3A"
--- BATCH_API_TEST_1.ps1
+++
@@ -1,6 +0,0 @@
-# Test 1 file
-$Files = @(
-    "api/shared/sessionManager/services/fraudDetectionService.js"
-)
-
-& .\BULK_CONSOLE_FIX_V4.ps1 -Files $Files -BatchName "API_TEST_FRAUD"
