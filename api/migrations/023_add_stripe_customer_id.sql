5602bc53af0b03ba1356403f53ed8f12528c714b
Stuart <stuathome87@gmail.com>
2026-01-16 21:16:50 -0600

    old file cleanup

--- api/migrations/023_add_stripe_customer_id.sql
+++
@@ -1,9 +0,0 @@
--- Add Stripe customer ID column to user_personal_info
--- This stores the Stripe customer ID for subscription management
-
-ALTER TABLE user_personal_info 
-ADD COLUMN IF NOT EXISTS stripe_customer_id VARCHAR(255) UNIQUE;
-
--- Create index for faster lookups
-CREATE INDEX IF NOT EXISTS idx_user_personal_info_stripe_id 
-ON user_personal_info(stripe_customer_id);
